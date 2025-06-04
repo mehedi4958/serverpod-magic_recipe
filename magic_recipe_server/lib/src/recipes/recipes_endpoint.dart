ecf8eb9d2b3d28d380ee3d216c9d613ab71d2e08
Mehedi Hasan <mehedi4958@gmail.com>
2025-06-04 09:16:51 +0800

    test written for generateRecipe

--- magic_recipe_server/lib/src/recipes/recipes_endpoint.dart
+++ magic_recipe_server/lib/src/recipes/recipes_endpoint.dart
@@ -2,11 +2,21 @@ import 'dart:async';
 
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:magic_recipe_server/src/generated/protocol.dart';
+import 'package:meta/meta.dart';
 import 'package:serverpod/serverpod.dart';
 
 /// This is the endpoint that will be used to generate a recipe using the
 /// Google Gemini API. It extends the Endpoint class and implements the
 /// generateRecipe method.
+
+@visibleForTesting
+var generateContent = (String apiKey, String prompt) async =>
+    (await GenerativeModel(model: 'gemini-2.0-flash', apiKey: apiKey)
+            .generateContent(
+      [Content.text(prompt)],
+    ))
+        .text;
+
 class RecipesEndpoint extends Endpoint {
   /// Pass in a string containing the ingredients and get a recipe back.
   Future<Recipe> generateRecipe(Session session, String ingredients) async {
@@ -16,10 +26,10 @@ class RecipesEndpoint extends Endpoint {
     if (geminiApiKey == null) {
       throw Exception('Gemini API key not found');
     }
-    final gemini = GenerativeModel(
-      model: 'gemini-2.0-flash',
-      apiKey: geminiApiKey,
-    );
+    // final gemini = GenerativeModel(
+    //   model: 'gemini-2.0-flash',
+    //   apiKey: geminiApiKey,
+    // );
 
     // A prompt to generate a recipe, the user will provide a free text input with the ingredients
     final prompt =
@@ -27,9 +37,9 @@ class RecipesEndpoint extends Endpoint {
         'of the recipe in the first line, and then the instructions. The recipe should be easy '
         'to follow and include all necessary steps. Please provide a detailed recipe.';
 
-    final response = await gemini.generateContent([Content.text(prompt)]);
+    // final response = await generateContent(geminiApiKey,prompt);
 
-    final responseText = response.text;
+    final responseText = await generateContent(geminiApiKey,prompt);
 
     // Check if the response is empty or null
     if (responseText == null || responseText.isEmpty) {
